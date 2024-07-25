5714ca7f84a2ff0428b9674c545e831943fe7634
kevinworthington <kworthin@gmail.com>
2024-07-25 11:15:15 -0600

    Updated job template time and email

--- job_template.sh
+++ job_template.sh
@@ -3,12 +3,12 @@
 #SBATCH --partition=amilan
 #SBATCH --job-name=cnrp
 #SBATCH --output=cnrp-job.%j.out
-#SBATCH --time=1:00:00
+#SBATCH --time=0:15:00
 #SBATCH --qos=normal
 #SBATCH --nodes=1
 #SBATCH --ntasks=4
 #SBATCH --mail-type=ALL
-#SBATCH --mail-user=akhilsin@colostate.edu
+#SBATCH --mail-user=kevin.worthington@colostate.edu
 
 
 module load anaconda
